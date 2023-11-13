19486772678c2fbac1befcbf353f93cba42dcbfc
YsN23 <yy2369964@gmail.com>
2023-11-13 18:29:25 +0100

    Added Some Modif to the free function

--- free_lmlawi.c
+++ free_lmlawi.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * free_lmlawi - frees an array of strings
- * @array: array of strings
+* free_lmlawi - frees an array of strings
+* @array: array of strings
 */
 
 
@@ -10,29 +10,9 @@ void free_lmlawi(char **array)
 {
 	int i = 0;
 
-	if (array == NULL)
-	{
-		while (array[i])
-		{
-			free(array[i]);
-			i++;
-		}
-	}
-	free(array[i]);
+	if (!array)
+		return;
+	for (; array[i]; i++)
+		free(array[i]);
 	free(array);
-}
-
-/**
- * call_env - call the env_print function
- * @arr: array
- * @str: string (env)
-*/
-
-void call_env(char **arr, char *str)
-{
-	if (_strcmp(arr[0], str) == 0)
-	{
-		env_print();
-		free(arr);
-	}
 }
